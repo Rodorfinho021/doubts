3ea87d21b3cdf6d8ca746e3327c54b01cc24171b
Rodorfinho021 <160938761+Rodorfinho021@users.noreply.github.com>
2025-06-04 16:01:13 -0400

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 import mysql from 'mysql2';
 
 const pool = mysql.createPool({
-  host: 'https://apidoubts.dev.vilhena.ifro.edu.br',
+  host: 'ss04ggwskkwc0ksgcookg48w',
   user: 'doubts',
   password: '12345678',
   database: 'doubts_db',
-  port: 20008,
+  port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
