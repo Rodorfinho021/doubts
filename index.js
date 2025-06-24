aa110925deab1a5093e2b41009df658a7085b303
Rodorfinho021 <rodolfo.bezerra@estudante.ifro.edu.br>
2025-06-24 18:29:55 -0400

    teste

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const allowedOrigins = [
   'https://doubts.dev.vilhena.ifro.edu.br',
-  'http://localhost:3000/'
+  'http://localhost:3000'
 ];
 
 app.use(cors({
